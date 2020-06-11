7621bf73ce7157a4ae2cd11e362d3f1bbe2063bc
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2020-06-11 17:54:16 +0200

    toString method implemented for fields

--- app/src/main/java/com/example/sznake/SnakeHead.java
+++ app/src/main/java/com/example/sznake/SnakeHead.java
@@ -1,4 +1,11 @@
 package com.example.sznake;
 
+import androidx.annotation.NonNull;
+
 public class SnakeHead extends SnakeBodyPart {
+    @NonNull
+    @Override
+    public String toString() {
+        return "O";
+    }
 }
--- app/src/main/java/com/example/sznake/SnakeTail.java
+++ app/src/main/java/com/example/sznake/SnakeTail.java
@@ -2,4 +2,9 @@ package com.example.sznake;
 
 public class SnakeTail extends SnakeBodyPart {
     SnakeBodyPart previousBodyPart;
+
+    @Override
+    public String toString() {
+        return "o";
+    }
 }
