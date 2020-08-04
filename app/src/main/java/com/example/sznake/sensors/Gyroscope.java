113f5bffce4b82beb4c30dae0d095f2e9a6d24a6
k-dom <kd.domanski@gmail.com>
2020-08-04 18:29:20 +0200

    Added gyroscope; Snake moves using gyroscope.

--- app/src/main/java/com/example/sznake/sensors/Gyroscope.java
+++ app/src/main/java/com/example/sznake/sensors/Gyroscope.java
@@ -6,18 +6,16 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
-public class Accelerometer extends MotionSensor {
+public class Gyroscope extends MotionSensor {
 
-
-
-    public Accelerometer(Context context) {
+    public Gyroscope(Context context) {
 
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if(listener != null) {
+                if(listener !=null) {
                     listener.onTranslation(event.values[0], event.values[1]);
                 }
             }
@@ -27,8 +25,5 @@ public class Accelerometer extends MotionSensor {
 
             }
         };
-
     }
-
 }
-
