5c6275540624f5dc59b324adffce8ffa20ceaa16
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-08-05 19:11:54 +0200

    Added createBorder method to Game class

--- app/src/main/java/com/example/sznake/Game.java
+++ app/src/main/java/com/example/sznake/Game.java
@@ -12,7 +12,6 @@ import java.util.Random;
 public class Game {
 
 
-
     private GameBoard gameBoard;
     int points = 0;
     private boolean isDead;
@@ -34,28 +33,50 @@ public class Game {
         GameField nextLocation = gameBoard.getSnakeNextLocation();
         int snakeX = nextLocation.getX();
         int snakeY = nextLocation.getY();
-        if(!snake.isGrowing()){
-            int tailX=snake.getLast().getX();
-            int tailY=snake.getLast().getY();
-            gameBoard.getFields()[tailX][tailY]= new EmptyField(tailX,tailY);
+        if (!snake.isGrowing()) {
+            int tailX = snake.getLast().getX();
+            int tailY = snake.getLast().getY();
+            gameBoard.getFields()[tailX][tailY] = new EmptyField(tailX, tailY);
         }
-        SnakeBodyPart snakeBodyPart = new SnakeBodyPart(snakeX,snakeY);
+        SnakeBodyPart snakeBodyPart = new SnakeBodyPart(snakeX, snakeY);
         snake.move(snakeBodyPart);
-        gameBoard.getFields()[snakeBodyPart.getX()][snakeBodyPart.getY()]=snakeBodyPart;
-            }
+        gameBoard.getFields()[snakeBodyPart.getX()][snakeBodyPart.getY()] = snakeBodyPart;
+    }
 
 
     public void generateUpgrade(PowerUp upgrade) {
         Random generator = new Random();
         int x = generator.nextInt(gameBoard.getSizeX());
         int y = generator.nextInt(gameBoard.getSizeY());
-        upgrade.setCoordinates(x,y);
+        upgrade.setCoordinates(x, y);
         if (gameBoard.get(x, y).getClass() == EmptyField.class) {
-            gameBoard.getFields()[x][y]=upgrade;
+            gameBoard.getFields()[x][y] = upgrade;
         } else {
             generateUpgrade(upgrade);
         }
 
+    }
+
+    public void createBorder(int borderType) {
+        int sizeX = gameBoard.getSizeX();
+        int sizeY = gameBoard.getSizeY();
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                if (borderType == 1) {
+                    if (i == 0 || j == 0 || i == sizeX - 1 || j == sizeY - 1) {
+                        BlockedField blockedField = new BlockedField();
+                        blockedField.setCoordinates(i, j);
+                        gameBoard.getFields()[i][j] = blockedField;
+                    }
+                } else if (borderType == 2) {
+                    if ((i == 0 || j == 0 || i == sizeX - 1 || j == sizeY - 1) && (i > (sizeX / 2) + 5 || i < (sizeX / 2) - 5) && (j > (sizeY / 2) + 5 || j < (sizeY / 2) - 5)) {
+                        BlockedField blockedField = new BlockedField();
+                        blockedField.setCoordinates(i, j);
+                        gameBoard.getFields()[i][j] = blockedField;
+                    }
+                }
+            }
+        }
     }
 
 
@@ -65,7 +86,7 @@ public class Game {
         Display.displayBoard(gameBoard);
         while (true) {
             Class<? extends GameField> nextFieldType = gameBoard.getSnakeNextLocation().getClass();
-            int x=generator.nextInt(2);
+            int x = generator.nextInt(2);
           /*  if(x==0){
                 gameBoard.getSnake().setOrientation(Orientation.LEFT);
             }
@@ -76,12 +97,12 @@ public class Game {
                 points++;
                 generateUpgrade(new GrowUp());
                 gameBoard.getSnake().setGrowing(true);
-            } else if (nextFieldType == BlockedField.class ||nextFieldType == SnakeBodyPart.class) {
+            } else if (nextFieldType == BlockedField.class || nextFieldType == SnakeBodyPart.class) {
                 System.out.println(gameBoard.getSnakeNextLocation().getX());
                 System.out.println(gameBoard.getSnakeNextLocation().getY());
                 break;
             }
-            
+
             moveSnake();
 
             Display.displayBoard(gameBoard);
@@ -98,7 +119,7 @@ public class Game {
             points++;
             generateUpgrade(new GrowUp());
             gameBoard.getSnake().setGrowing(true);
-        } else if (nextFieldType == BlockedField.class ||nextFieldType == SnakeBodyPart.class) {
+        } else if (nextFieldType == BlockedField.class || nextFieldType == SnakeBodyPart.class) {
             isDead = true;
         }
 
@@ -108,10 +129,10 @@ public class Game {
     public void draw(Canvas canvas, SurfaceHolder surfaceHolder, Paint paint, int blockSize) {
 
 //        canvas = surfaceHolder.lockCanvas();
-        canvas.drawColor(Color.argb(255,26,128, 182));
+        canvas.drawColor(Color.argb(255, 26, 128, 182));
 
         for (GameField[] rows : gameBoard.getFields()) {
-            for( GameField field : rows) {
+            for (GameField field : rows) {
                 field.draw(canvas, surfaceHolder, paint, blockSize);
             }
 
