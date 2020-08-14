73555a45ac1c9eec146611bf637136c15470f0d7
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2020-08-14 10:50:47 +0200

    fast direction changing bug fixed

--- app/src/main/java/com/example/sznake/utils/Direction.java
+++ app/src/main/java/com/example/sznake/utils/Direction.java
@@ -6,25 +6,47 @@ import java.util.Random;
 
 public enum Direction {
     UP, DOWN, LEFT, RIGHT;
-    public static Direction getRandomElement(){
+
+    public static Direction getRandomElement() {
         Random random = new Random();
         return values()[random.nextInt(values().length)];
     }
+
     @NonNull
     @Override
-    public String toString(){
-        if(this.equals(UP)){
+    public String toString() {
+        if (this.equals(UP)) {
             return "^";
         }
-        if(this.equals(DOWN)){
+        if (this.equals(DOWN)) {
             return "V";
         }
-        if(this.equals(LEFT)){
+        if (this.equals(LEFT)) {
             return "<";
         }
-        if(this.equals(RIGHT)){
+        if (this.equals(RIGHT)) {
             return ">";
         }
         return "";
+    }
+
+    public boolean isOpposite(Direction direction) {
+
+        switch (this) {
+            case UP:
+                return direction == Direction.DOWN;
+
+            case DOWN:
+                return direction == Direction.UP;
+
+            case LEFT:
+                return direction == Direction.RIGHT;
+
+            case RIGHT:
+                return direction == Direction.LEFT;
+
+            default:
+                return false;
+        }
     }
 }
