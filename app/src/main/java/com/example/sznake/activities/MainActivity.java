d8d4d6f028065bc0aa93bd7f2dc1ada1c7e46645
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2020-08-14 16:48:24 +0200

    sound support

--- app/src/main/java/com/example/sznake/activities/MainActivity.java
+++ app/src/main/java/com/example/sznake/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -14,11 +15,30 @@ import com.example.sznake.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer music;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        music = MediaPlayer.create(MainActivity.this, R.raw.vandetta);
+        music.setLooping(true);
+        findViewById(R.id.volumeCtrl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(music.isPlaying())
+                {
+                    music.pause();
+                    v.setBackgroundResource(R.drawable.ic_baseline_volume_up_24);
+                } else
+                {
+                    music.start();
+                    v.setBackgroundResource(R.drawable.ic_baseline_volume_off_24);
+                }
+            }
+        });
 
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +51,11 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
             this.startActivity(intent);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        music.release();
     }
 }
