f944b48dfec6dcec2a9a61c5f8533f77aa6a14c8
k-dom <kd.domanski@gmail.com>
2020-08-19 15:41:45 +0200

    Improved brightness adjustment.

--- app/src/main/java/com/example/sznake/sensorServices/LightService.java
+++ app/src/main/java/com/example/sznake/sensorServices/LightService.java
@@ -7,6 +7,9 @@ import android.provider.Settings;
 
 public class LightService extends SensorService {
 
+    private static final float LIGHT_LOW = 0f;
+    private static final float LIGHT_MEDIUM = 200f;
+    private static final float LIGHT_HIGH = 1000f;
     private int currentScreenBrightness;
 
     public LightService(Context context) throws Settings.SettingNotFoundException {
@@ -18,11 +21,21 @@ public class LightService extends SensorService {
     @Override
     public void onTranslation(float[] sensorEventValues) {
         float valX = sensorEventValues[0];
-        if (valX > 1000) {
-            currentScreenBrightness = 255;
-        }
+        setCurrentScreenBrightness(valX);
     }
 
+    private void setCurrentScreenBrightness(float value) {
+        if (value > LIGHT_HIGH) {
+            currentScreenBrightness = 255;
+        }
+        else if (value > LIGHT_LOW && value < LIGHT_MEDIUM) {
+            currentScreenBrightness = 125;
+        }
+        else if (value == LIGHT_LOW) {
+            currentScreenBrightness = 25;
+        }
+
+    }
     public int getCurrentScreenBrightness() {
         return currentScreenBrightness;
     }
