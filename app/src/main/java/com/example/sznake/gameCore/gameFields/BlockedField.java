8502ec7767a6c0996f814e9db4df233be69da7c4
k-dom <kd.domanski@gmail.com>
2020-08-20 17:11:42 +0200

    Javadoc BlockedField.

--- app/src/main/java/com/example/sznake/gameCore/gameFields/BlockedField.java
+++ app/src/main/java/com/example/sznake/gameCore/gameFields/BlockedField.java
@@ -2,17 +2,27 @@ package com.example.sznake.gameCore.gameFields;
 
 import android.graphics.Color;
 
-import androidx.annotation.NonNull;
-
+/**
+ * Represents {@link GameField} type that blocks the movement and upon colliding
+ * with {@link com.example.sznake.gameCore.Snake} ends the game.
+ * 
+ * @see GameField
+ * @see com.example.sznake.gameCore.Snake
+ */
 public class BlockedField extends GameField {
+    /**
+     * Creates a new BlockedField with specified coordinates
+     * and default black color.
+     * <p>
+     * Calls parent's constructor.
+     * 
+     * @param x  field coordinate along x axis
+     * @param y  field coordinate along y axis
+     * @see GameField#GameField(int, int, int) 
+     */
     public BlockedField(int x, int y)
     {
         super(x, y, Color.BLACK);
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return "#";
-    }
 }
