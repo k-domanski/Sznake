857a0d7ff4cca91c1b998c3a05e07fb24e865282
k-dom <kd.domanski@gmail.com>
2020-08-20 22:21:29 +0200

    Javadoc EmptyField.

--- app/src/main/java/com/example/sznake/gameCore/gameFields/EmptyField.java
+++ app/src/main/java/com/example/sznake/gameCore/gameFields/EmptyField.java
@@ -2,16 +2,24 @@ package com.example.sznake.gameCore.gameFields;
 
 import android.graphics.Color;
 
-import androidx.annotation.NonNull;
-
+/**
+ * Represents an empty type of {@link GameField}.
+ * 
+ * @see GameField
+ */
 public class EmptyField extends GameField {
+    /**
+     * Creates a new EmptyField with specified coordinates
+     * and default color.
+     * <p>
+     * Calls parent's constructor.
+     * 
+     * @param x  coordinate along x axis
+     * @param y  coordinate along y axis
+     * @see GameField#GameField(int, int, int) 
+     */
     public EmptyField(int x, int y) {
         super(x, y, Color.argb(255,255,255,210));
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return " ";
-    }
 }
