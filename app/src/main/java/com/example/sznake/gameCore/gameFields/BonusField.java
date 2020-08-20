02525b444656aaa5d99bfab499ae95e7b1b674f8
k-dom <kd.domanski@gmail.com>
2020-08-20 16:49:23 +0200

    Javadoc BonusField

--- app/src/main/java/com/example/sznake/gameCore/gameFields/BonusField.java
+++ app/src/main/java/com/example/sznake/gameCore/gameFields/BonusField.java
@@ -1,6 +1,23 @@
 package com.example.sznake.gameCore.gameFields;
 
+/**
+ * BonusField is an abstract class that represents
+ * any type of GameField that influences game play.
+ *
+ * @see GameField
+ * @see GrowUpField
+ */
 public abstract class BonusField extends GameField {
+    /**
+     * Creates a new BonusField with specified coordinates and color.
+     * <p>
+     * Calls parent's constructor.
+     * 
+     * @param x      field coordinate along x axis
+     * @param y      field coordinate along y axis
+     * @param color  field color
+     * @see GameField#GameField(int, int, int) 
+     */
     protected BonusField(int x, int y, int color) {
         super(x, y, color);
     }
