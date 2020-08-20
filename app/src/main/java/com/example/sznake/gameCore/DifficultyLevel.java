860cf9a4382fa931e768647b7c3505f1c0200ac2
k-dom <kd.domanski@gmail.com>
2020-08-20 22:43:47 +0200

    Javadoc DifficultyLevel.

--- app/src/main/java/com/example/sznake/gameCore/DifficultyLevel.java
+++ app/src/main/java/com/example/sznake/gameCore/DifficultyLevel.java
@@ -1,7 +1,22 @@
 package com.example.sznake.gameCore;
 
+/**
+ * Represents the difficulty level of a game.
+ */
 public enum DifficultyLevel {
+    /**
+     * There are no {@link com.example.sznake.gameCore.gameFields.BlockedField}
+     * on the {@link GameBoard}
+     */
     EASY,
+    /**
+     * There are some {@link com.example.sznake.gameCore.gameFields.BlockedField}
+     * on the edges of the{@link GameBoard}.
+     */
     MEDIUM,
+    /**
+     * All the edges of {@link GameBoard} are made with
+     * {@link com.example.sznake.gameCore.gameFields.BlockedField}
+     */
     HARD
 }
