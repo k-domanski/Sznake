c64f9b39e0b5b35aa0ecfb305a11a8f01338d4da
k-dom <kd.domanski@gmail.com>
2020-08-20 17:00:31 +0200

    Javadoc GrowUpField.

--- app/src/main/java/com/example/sznake/gameCore/gameFields/GrowUpField.java
+++ app/src/main/java/com/example/sznake/gameCore/gameFields/GrowUpField.java
@@ -2,16 +2,23 @@ package com.example.sznake.gameCore.gameFields;
 
 import android.graphics.Color;
 
-import androidx.annotation.NonNull;
-
+/**
+ * Represents a type of {@link BonusField} that upon
+ * colliding with {@link com.example.sznake.gameCore.Snake}
+ * makes it bigger and adds points to player.
+ */
 public class GrowUpField extends BonusField {
+    /**
+     * Creates a new GrowUpField with specified coordinates
+     * and default green color.
+     * <p>
+     * Calls parent's constructor.
+     *
+     * @param x coordinate along x axis
+     * @param y coordinate along y axis
+     */
     public GrowUpField(int x, int y) {
         super(x, y, Color.GREEN);
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return "G";
-    }
 }
