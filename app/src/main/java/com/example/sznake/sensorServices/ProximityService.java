56c968bfd2ff9fc63ba5065f9abbeb9501cce18c
k-dom <kd.domanski@gmail.com>
2020-08-23 18:53:46 +0200

    Javadoc ProximitySensor.

--- app/src/main/java/com/example/sznake/sensorServices/ProximityService.java
+++ app/src/main/java/com/example/sznake/sensorServices/ProximityService.java
@@ -7,31 +7,48 @@ import android.hardware.SensorManager;
 
 import com.example.sznake.gameCore.DifficultyLevel;
 
+/**
+ * Represents the proximity sensor.
+ * <p>
+ * It is responsible for difficulty level change based on output from proximity sensor.
+ */
 public class ProximityService extends SensorService {
-
-    boolean isPaused;
+    /**
+     * Difficulty level based on proximity sensor
+     */
     private DifficultyLevel difficultyLevel = DifficultyLevel.EASY;
 
+    /**
+     * Creates a new ProximityService with Proximity sensor type and normal sampling period.
+     *
+     * @param context   specified {@link Context}
+     * @see Sensor#TYPE_PROXIMITY
+     * @see SensorManager#SENSOR_DELAY_NORMAL
+     */
     public ProximityService(Context context) {
         super(context, Sensor.TYPE_PROXIMITY, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    /**
+     * Changes difficulty based on monitored sensor.
+     * <p>
+     * Whenever the value of proximity sensor is 0, difficulty switches to next one.
+     *
+     * @param sensorEventValues values of monitored sensor.
+     * @see DifficultyLevel
+     */
     @Override
     public void onTranslation(float[] sensorEventValues) {
         float valX = sensorEventValues[0];
-//        if(valX < sensor.getMaximumRange()) {
-//            isPaused = !isPaused;
-//        }
         if (valX == 0) {
             difficultyLevel = difficultyLevel.getNextLevel();
         }
     }
 
-    public boolean isPaused()
-    {
-        return isPaused;
-    }
-
+    /**
+     *
+     * @return  current difficulty level
+     */
     public DifficultyLevel getDifficultyLevel() {
         return difficultyLevel;
     }
