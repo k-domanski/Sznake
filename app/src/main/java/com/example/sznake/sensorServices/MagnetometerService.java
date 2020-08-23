6047dde9ea1861dde78c268c86785187375925f0
k-dom <kd.domanski@gmail.com>
2020-08-23 20:35:50 +0200

    Javadoc MagnetometerService.

--- app/src/main/java/com/example/sznake/sensorServices/MagnetometerService.java
+++ app/src/main/java/com/example/sznake/sensorServices/MagnetometerService.java
@@ -6,19 +6,53 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import java.lang.Math;
 
+/**
+ * Represents the Magnetometer sensor.
+ * <p>
+ * Responsible for choosing random position values based on magnetometer values.
+ * Positions are used to spawn {@link com.example.sznake.gameCore.gameFields.GrowUpField}
+ * on the game board.
+ */
 public class MagnetometerService extends SensorService {
-
+    /**
+     * Position on the x-axis.
+     */
     int m_randX = 0;
+    /**
+     * Position on the y-axis.
+     */
     int m_randY = 0;
+    /**
+     * Screen width in pixels.
+     */
     int m_screenWidth;
+    /**
+     * Screen height in pixels.
+     */
     int m_screenHeight;
 
+    /**
+     * Creates new MagnetometerService with specified screen width and screen height;
+     * sensor type is magnetic field and sensor delay normal.
+     *
+     * @param context       specified {@link Context}
+     * @param screenWidth   screen width in pixels
+     * @param screenHeight  screen height in pixels
+     */
     public MagnetometerService(Context context, int screenWidth, int screenHeight) {
         super(context, Sensor.TYPE_MAGNETIC_FIELD, SensorManager.SENSOR_DELAY_NORMAL);
         m_screenHeight = screenHeight;
         m_screenWidth = screenWidth;
     }
 
+    /**
+     * Sets random position along x and y axis.
+     * <p>
+     * Based on magnetometer's values of ambient magnetic field in x and y axis and
+     * current screen width and height.
+     *
+     * @param sensorEventValues values of monitored sensor.
+     */
     @Override
     public void onTranslation(float[] sensorEventValues) {
         m_randX = (100 * Math.abs(Math.round(sensorEventValues[0]))) % m_screenWidth;
