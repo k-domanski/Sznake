328432815bbd87528b67268072d2337c763a1226
k-dom <kd.domanski@gmail.com>
2020-08-23 20:02:23 +0200

    Javadoc GyroscopeService.

--- app/src/main/java/com/example/sznake/sensorServices/GyroscopeService.java
+++ app/src/main/java/com/example/sznake/sensorServices/GyroscopeService.java
@@ -6,19 +6,62 @@ import android.hardware.SensorManager;
 
 import com.example.sznake.utils.Direction;
 
+/**
+ * Represents the gyroscope sensor.
+ * <p>
+ * It is responsible for changing {@link Direction}
+ * based on output from gyroscope sensor.
+ */
 public class GyroscopeService extends SensorService {
 
+    /**
+     * Constant represents sensitivity treshold of sensor values.
+     */
     private static final float GYRO_TRESHOLD = 3.0f;
+    /**
+     * Direction based on gyroscope output.
+     */
     private Direction m_direction = Direction.UP;
 
+    /**
+     * Creates new GyroscopeService with gyroscope sensor type and game sensor delay.
+     *
+     * @param context  specified {@link Context}
+     */
     public GyroscopeService(Context context) {
         super(context, Sensor.TYPE_GYROSCOPE, SensorManager.SENSOR_DELAY_GAME);
     }
 
+    /**
+     * Called whenever gyroscope sensor values change.
+     * Passes sensor values to {@link #changeDirection(float, float)}.
+     *
+     * @param sensorEventValues values of monitored sensor.
+     */
     @Override
     public void onTranslation(float[] sensorEventValues) {
         float valX = sensorEventValues[0];
         float valY = sensorEventValues[1];
+        changeDirection(valX, valY);
+    }
+
+    /**
+     * Changes direction based on gyroscope values.
+     * <p>
+     * If the valX parameter is positive and greater than {@link #GYRO_TRESHOLD}
+     * direction changes to RIGHT;
+     * if valX parameter is negative and smaller than negative value of {@link #GYRO_TRESHOLD}
+     * direction changes to LEFT.
+     * If the valY parameter is positive and greater than {@link #GYRO_TRESHOLD}
+     * direction changes to UP;
+     * if valY parameter is negative and smaller than negative value of {@link #GYRO_TRESHOLD}
+     * direction changes to DOWN.
+     *
+     *
+     * @param valX  angular speed around the x-axis
+     * @param valY  angular speed around the y-axis
+     */
+    private void changeDirection(float valX, float valY) {
         if (valY > GYRO_TRESHOLD) {
             m_direction = Direction.UP;
         } else if (valY < -GYRO_TRESHOLD) {
@@ -29,7 +72,6 @@ public class GyroscopeService extends SensorService {
             m_direction = Direction.LEFT;
         }
     }
-
 
     public Direction getDirection() {
         return m_direction;
