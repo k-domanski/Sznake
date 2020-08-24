e22dba648596809e5645c0520952dbafb49781ac
k-dom <kd.domanski@gmail.com>
2020-08-25 00:59:49 +0200

    Javadoc Snake.

--- app/src/main/java/com/example/sznake/gameCore/Snake.java
+++ app/src/main/java/com/example/sznake/gameCore/Snake.java
@@ -7,12 +7,33 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represent the snake.
+ * <p>
+ * Snake consists of {@link SnakeField} and moves in specified direction.
+ */
 public class Snake implements Serializable {
-
+    /**
+     * List of Snakes body parts.
+     */
     private List<SnakeField> body;
+    /**
+     * The {@link Direction} the snake is moving.
+     */
     private Direction direction;
+    /**
+     * Tracks if the Snake growth.
+     */
     private boolean isGrowing;
 
+    /**
+     * Creates an new Snake with specified length, direction and position on the board.
+     *
+     * @param length     the starting length of snake
+     * @param direction  the direction in which snakes initially moves
+     * @param X          initial position on the board along x-axis
+     * @param Y          initial position on the board along y-axis
+     */
     public Snake(int length, Direction direction, int X, int Y) {
         body = new ArrayList<>();
         for (int i = 0; i < length; i++) {
@@ -33,6 +54,14 @@ public class Snake implements Serializable {
 
     }
 
+    /**
+     * Moves Snake.
+     * <p>
+     * Adds a {@link SnakeField} to the beginning of Snake.
+     * If Snake is not suppose to grow removes the last part of Snakes body.
+     *
+     * @param snakeField  new {@link SnakeField}
+     */
     public void move(SnakeField snakeField) {
         body.add(0, snakeField);
         if (!isGrowing) {
@@ -42,10 +71,21 @@ public class Snake implements Serializable {
 
     }
 
+    /**
+     * Gets the specified {@link SnakeField} from the Snakes body.
+     *
+     * @param x  index of the {@link SnakeField} element in the Snake body
+     * @return   {@link SnakeField} from the Snake body list
+     */
     public SnakeField get(int x) {
         return body.get(x);
     }
 
+    /**
+     * Returns the last Snakes body part.
+     *
+     * @return  the last element from the body list.
+     */
     public SnakeField getLast() {
         return body.get(body.size() - 1);
     }
