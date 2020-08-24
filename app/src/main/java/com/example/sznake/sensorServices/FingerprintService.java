71550ee1e5c53f9f28d1a4362eb1a4c20311a717
Micho6 <michal.dudkiewicz96@gmail.com>
2020-08-24 16:53:22 +0200

    fingerprint documentation

--- app/src/main/java/com/example/sznake/sensorServices/FingerprintService.java
+++ app/src/main/java/com/example/sznake/sensorServices/FingerprintService.java
@@ -7,30 +7,46 @@ import android.os.CancellationSignal;
 
 import androidx.annotation.RequiresApi;
 
+/**
+ * Represents the fingerprint sensor service.
+ * <p>
+ * It is responsible for changing background music in an activity,
+ * which implements listener listening to any changes in the sensor
+ */
 public class FingerprintService {
     private FingerprintManager m_fingerprintManager;
     private CancellationSignal m_cancellationSignal;
     private FingerprintManager.AuthenticationCallback m_authenticationCallback;
     private OnAuthenticationListener m_authorisationListener;
 
+    /**
+     * Iterface implemented by an activity
+     * that needs to react to changes in fingerprint sensor.
+     * The reaction must be implemented in onAuth() method.
+     */
     public interface OnAuthenticationListener {
         void onAuth();
     }
 
+    /**
+     * Setter used for setting up the listener
+     * @param listener listener of fingerprint authentication
+     */
     public void setOnAuthenticationListener(OnAuthenticationListener listener){
         m_authorisationListener = listener;
     }
 
+    /**
+     * Creates new FingerprintService with specified context.
+     *
+     * @param context  specified {@link Context}
+     */
     public FingerprintService(Context context){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             m_fingerprintManager = context.getSystemService(FingerprintManager.class);
             m_cancellationSignal = new CancellationSignal();
 
             m_authenticationCallback = new FingerprintManager.AuthenticationCallback() {
-                @Override
-                public void onAuthenticationError(int errorCode, CharSequence errString) {
-                    // do not implement unless You know what You are doing
-                }
 
                 @Override
                 public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
@@ -50,6 +66,9 @@ public class FingerprintService {
         }
     }
 
+    /**
+     * Called whenever listener starts listening.
+     */
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void startListening(){
         if (isFingerScannerAvailableAndSet() ) {
@@ -63,6 +82,9 @@ public class FingerprintService {
         }
     }
 
+    /**
+     * Called whenever listener stops listening.
+     */
     public void stopListening(){
         if ( isFingerScannerAvailableAndSet() ) {
             try {
