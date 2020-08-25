3482b1060f0f45d5ecb20f94e585459b2bbcfc76
k-dom <kd.domanski@gmail.com>
2020-08-25 23:41:54 +0200

    Javadoc GameView.

--- app/src/main/java/com/example/sznake/view/GameView.java
+++ app/src/main/java/com/example/sznake/view/GameView.java
@@ -22,8 +22,10 @@ import com.example.sznake.sensorServices.ProximityService;
 
 import java.beans.PropertyChangeSupport;
 
+/**
+ * 
+ */
 public class GameView extends SurfaceView implements Runnable {
-
     private static final long MILLIS_PER_SECOND = 1000;
     private static final long FPS = 10;
     private static final int NUM_BLOCKS_WIDE = 40;
@@ -55,8 +57,18 @@ public class GameView extends SurfaceView implements Runnable {
     private SurfaceHolder surfaceHolder;
     private Paint paint;
 
-
-    public GameView(Context context, Point size, Game game,DifficultyLevel difficultyLevel) {
+    /**
+     * Creates a new GameView with specified screen size, game and difficulty level.
+     * <p>
+     * Creates all the required {@link com.example.sznake.sensorServices.SensorService},
+     * starts a new game or loads an existing one.
+     *
+     * @param context          specified {@link Context}
+     * @param size             screen size
+     * @param game             <code>null</code> if new game, otherwise game to be loaded.
+     * @param difficultyLevel  desired difficulty level of the game
+     */
+    public GameView(Context context, Point size, Game game, DifficultyLevel difficultyLevel) {
         super(context);
         screenX = size.x;
         screenY = size.y;
@@ -81,7 +93,7 @@ public class GameView extends SurfaceView implements Runnable {
         magnetometerService = new MagnetometerService(context, NUM_BLOCKS_WIDE, numBlocksHigh);
 
         changeSupport = new PropertyChangeSupport(this);
-        if(game==null) {
+        if(game == null) {
             newGame(difficultyLevel);
         }
         else {
@@ -89,14 +101,31 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
-
+    /**
+     * Contains games main loop.
+     * <p>
+     * Updates the position of a {@link com.example.sznake.gameCore.gameFields.GrowUpField}.
+     * Updates the game {@link Game#update()}.
+     * Updates {@link com.example.sznake.gameCore.Snake} position based on
+     * {@link GyroscopeService}.
+     * If quick time event is active checks if event is successful based on
+     * {@link AccelerometerService}.
+     * Updates the current screen brightness based on {@link LightService}.
+     * <p>
+     * Draws game board onto the screen {@link Game#draw(Canvas, Paint, int)}.
+     * Draws points onto the screen {@link #drawPoints()}.
+     * If {@link com.example.sznake.gameCore.QTE} is active then draws it onto the screen.
+     * Checks if {@link Game} is over.
+     */
     @Override
     public void run() {
         while (isPlaying) {
             if (isUpdateRequired()) {
                 game.setUpgradePosition(magnetometerService.getRandX(),
                         magnetometerService.getRandY());
+
                 game.update();
+
                 if(!game.getGameBoard().getSnake().getDirection().isOpposite(
                         gyroscopeService.getDirection())) {
                     game.getGameBoard().getSnake().setDirection(gyroscopeService.getDirection());
@@ -113,7 +142,9 @@ public class GameView extends SurfaceView implements Runnable {
 
                 if (surfaceHolder.getSurface().isValid()) {
                     canvas = surfaceHolder.lockCanvas();
+
                     game.draw(canvas, paint, blockSize);
+
                     drawPoints();
 
                     if(game.isQTEActive()) {
@@ -132,16 +163,10 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
-    private void drawPoints() {
-        String points;
-        points = String.valueOf(game.getPoints());
-        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-        paint.setColor(Color.BLACK);
-        paint.setAlpha(60);
-        paint.setTextSize(100);
-        canvas.drawText(points, 20, 100, paint);
-    }
-
+    /**
+     * Resumes game. Registers all {@link com.example.sznake.sensorServices.SensorService}.
+     * Starts a new {@link Thread}.
+     */
     public void resume() {
         gyroscopeService.register();
         proximityService.register();
@@ -153,7 +178,9 @@ public class GameView extends SurfaceView implements Runnable {
         thread.start();
     }
 
-
+    /**
+     * Pauses the game. Unregisters all {@link com.example.sznake.sensorServices.SensorService}.
+     */
     public void pause() {
         try {
             gyroscopeService.unregister();
@@ -168,6 +195,12 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
+    /**
+     * Checks if the update is required.
+     *
+     * @return  <code>true</code> if the next frame time is lower or equal to the
+     *          current time in millis, otherwise <code>false</code>
+     */
     public boolean isUpdateRequired() {
         if(nextFrameTime <= System.currentTimeMillis()) {
             nextFrameTime = System.currentTimeMillis() + MILLIS_PER_SECOND / FPS;
@@ -176,31 +209,12 @@ public class GameView extends SurfaceView implements Runnable {
         return false;
     }
 
-    public boolean isGameOver() {
-        return gameOver;
-    }
-
-    private void newGame(DifficultyLevel difficultyLevel) {
-        game = new Game(NUM_BLOCKS_WIDE, numBlocksHigh, 5, gyroscopeService.getDirection(),difficultyLevel);
-        nextFrameTime = System.currentTimeMillis();
-    }
-    private void loadGame(Game game) {
-        this.game = game;
-        nextFrameTime = System.currentTimeMillis();
-    }
-
-    public PropertyChangeSupport getChangeSupport() {
-        return changeSupport;
-    }
-
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
             if (event.getX() >= screenX / 2 && (game.getGameBoard().getSnake().getDirection() == Direction.DOWN || game.getGameBoard().getSnake().getDirection() == Direction.UP)) {
                 gyroscopeService.setDirection(Direction.RIGHT);
-            } else if (event.getX() < screenX / 2 && (game.getGameBoard().getSnake().getDirection() == Direction.DOWN || game.getGameBoard().getSnake().getDirection() == Direction.UP)) {
+            } else if (event.getX() < (float)(screenX / 2) && (game.getGameBoard().getSnake().getDirection() == Direction.DOWN || game.getGameBoard().getSnake().getDirection() == Direction.UP)) {
                 gyroscopeService.setDirection(Direction.LEFT);
             } else if (event.getY() >= screenY / 2 && (game.getGameBoard().getSnake().getDirection() == Direction.RIGHT || game.getGameBoard().getSnake().getDirection() == Direction.LEFT)) {
                 gyroscopeService.setDirection(Direction.DOWN);
@@ -208,9 +222,51 @@ public class GameView extends SurfaceView implements Runnable {
                 gyroscopeService.setDirection(Direction.UP);
             }
         }
-
-
         return true;
+    }
+
+
+    /**
+     * Creates a new {@link Game} with specified difficulty.
+     *
+     * @param difficultyLevel desired difficulty level of a game.
+     * @see DifficultyLevel
+     */
+    private void newGame(DifficultyLevel difficultyLevel) {
+        game = new Game(NUM_BLOCKS_WIDE, numBlocksHigh, 5,
+                gyroscopeService.getDirection(),difficultyLevel);
+        nextFrameTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Loads a game.
+     *
+     * @param game  {@link Game} to be loaded
+     */
+    private void loadGame(Game game) {
+        this.game = game;
+        nextFrameTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Draws the current points value onto the screen.
+     */
+    private void drawPoints() {
+        String points;
+        points = String.valueOf(game.getPoints());
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        paint.setColor(Color.BLACK);
+        paint.setAlpha(60);
+        paint.setTextSize(100);
+        canvas.drawText(points, 20, 100, paint);
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public PropertyChangeSupport getChangeSupport() {
+        return changeSupport;
     }
 
     public Game getGame() {
