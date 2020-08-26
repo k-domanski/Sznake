0fdf15f504643e2981422dafaedcb484171c2f13
Micho6 <michal.dudkiewicz96@gmail.com>
2020-08-26 21:17:55 +0200

    GameOverActivity refactoring

--- app/src/main/java/com/example/sznake/activities/GameOverActivity.java
+++ app/src/main/java/com/example/sznake/activities/GameOverActivity.java
@@ -2,7 +2,6 @@ package com.example.sznake.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
